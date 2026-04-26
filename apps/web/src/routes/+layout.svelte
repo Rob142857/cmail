d230d6049f9efe5c1455d4a22fb7a7224796945a
Rob Evans <rme142857@gmail.com>
2026-04-26 10:40:37 +1000

    Upgrade to latest: vite 8, svelte-plugin 7, adapter-cloudflare 7, typescript 6

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,14 +1,11 @@
 <script>
   import '../app.css';
+  import { page } from '$app/state';
   let { children } = $props();
 </script>
 
 <svelte:head>
-  <title>{$page?.data?.appName || 'cmail'}</title>
+  <title>{page.data?.appName || 'cmail'}</title>
 </svelte:head>
 
 {@render children()}
-
-<script module>
-  import { page } from '$app/state';
-</script>
